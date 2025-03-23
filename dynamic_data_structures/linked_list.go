cb3d946e4540ffa471a28bfd076b6db14804ad75
Francesco Messina <iframe@live.it>
2025-03-23 16:36:58 +0100

    feat: stack

--- dynamic_data_structures/linked_list.go
+++ dynamic_data_structures/linked_list.go
@@ -1,20 +1,23 @@
-package main 
+package linkedlist
 import "fmt"
 
-type Node struct {
-	value int
+type Node[T any] struct {
+	value T
 	next *Node
+	prev *Node
 }
 
-func (ll *Node) Print(){
+func (ll *Node[T]) Print(){
+
 	for ll != nil {
 		fmt.Println(ll.value)
 		ll = ll.next
 	}
 }
 
-func (ll *Node) Len() int {
+func (ll *Node[T]) Len() int {
 	i := 0
+	
 	for ll != nil {
 		ll = ll.next
 		i++
@@ -23,20 +26,77 @@ func (ll *Node) Len() int {
 }
 
 
-
-func main () {
-	ll := &Node{value:0}
+func Append[T any](ll *Node[T], new *Node[T]) *Node[T] {
 	head := ll
-
-	for j := 1; j<10; j++ {
-		ll.next = &Node{value:j}
+	
+	for ll.next != nil {
 		ll = ll.next
+		i++
 	}
-
-	fmt.Printf("Last Node:\n")
-	ll.Print()
-	fmt.Printf("Head:\n")
-	head.Print()
-	head.Print()
-	fmt.Printf("List lenght: %d \n", head.Len())
+	ll.next = new
+	return head
 }
+
+
+func InsertAfter[T any](pr *Node[T], new *Node[T]) *Node {
+	tmp := pr.next
+	pr.next = new
+	new.next = tmp
+}
+
+
+func LinkedListInsert[T any](head *Node[T], i int, val T) *Node[T]{
+	new := &Node{value:val}
+	if i == 0{
+		new.next = head
+		return new
+	}
+	current := head
+	for j:=0; j!= i && current; j++{
+		current = current.next
+	}
+	if !current {
+		return nil
+	}
+	tmp = current.next
+	current.next = new
+	new.next = tmp
+	return head
+}
+
+
+func LinkedListInsertDelete[T any] (head *Node[T], i int){
+	if i == 0{
+		head = head.next
+		return head
+	}
+	prev := head 
+	current:= head
+
+	for j:=0; j!= i && current; j++{
+		prev = current
+		current = current.next
+	}
+	if !current { 
+		return nil 
+	}
+	prev.next = current.next
+	return head
+}
+
+// func test () {
+// 	ll := &Node{value:0}
+// 	head := ll
+
+// 	for j := 1; j<10; j++ {
+// 		ll.next = &Node{value:j}
+// 		ll = ll.next
+// 	}
+
+// 	fmt.Printf("Last Node:\n")
+// 	ll.Print()
+// 	fmt.Printf("Head:\n")
+// 	head.Print()
+// 	head.Print()
+// 	fmt.Printf("List lenght: %d \n", head.Len())
+// }
---
+++ stacks and queues/stacks.go
@@ -0,0 +1,10 @@
+package stack
+
+import "fmt"
+
+
+type Stack struct {
+	size: int
+	top: int
+	A := []
+}
